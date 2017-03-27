bc26d2015aeb3bf61326a40a251c8f80b9c9e4c4
julian cobo <juli_c_s1996@hotmail.com>
2017-03-27 19:33:36 +0200

    [NEW] ENTREGA FASE 1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,48 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Principal'});
 });
+
+router.get('/tareas', function(req, res, next) {
+  res.render('tareas', { title: 'Tareas'});
+});
+
+router.get('/tareas/new', function(req, res, next) {
+  res.render('newTarea', { title: 'Crear Tarea'});
+});
+
+
+router.post('/tareas/crear', function(req, res) {
+  res.render('newTarea', { title: 'Crear Tarea'});
+});
+
+
+
+
+var tareas = [
+{
+"nom":"Diseño wireframe",
+"etiquetes": [ "wireframe","app","hibrida"],
+"realització":"36"
+},
+{
+"nom":"Reunión",
+"etiquetes": ["app","hibrida"],
+"realització":"36"
+},
+{
+"nom":"Desarrollo Layout",
+"etiquetes": [ "layout","app","hibrida"],
+"realització":"36"
+},
+{
+"nom":"Reunión cliente",
+"etiquetes": [ "reunión","app","hibrida"],
+"realització":"36"
+}
+]
+
+
 
 module.exports = router;
