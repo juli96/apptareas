7237d8ab71259b2ca3be6d00f6db01a5e3c9433a
julian cobo <juli_c_s1996@hotmail.com>
2017-03-30 15:49:27 +0200

    [NEW] ENTREGA FASE 2

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var tareasArray = [
+    {
+        "nom":"Diseño wireframe",
+        "etiquetes": [ "wireframe","app","hibrida"],
+        "realitzacio":"36"
+    },
+    {
+        "nom":"Reunión",
+        "etiquetes": ["app","hibrida"],
+        "realitzacio":"36"
+    },
+    {
+        "nom":"Desarrollo Layout",
+        "etiquetes": [ "layout","app","hibrida"],
+        "realitzacio":"36"
+    },
+    {
+        "nom":"Reunión cliente",
+        "etiquetes": [ "reunión","app","hibrida"],
+        "realitzacio":"36"
+    }
+    ];
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Principal'});
+  res.render('index', { title: 'Bienvenido a la pagina principal del proyecte Node'});
 });
 
 router.get('/tareas', function(req, res, next) {
-  res.render('tareas', { title: 'Tareas'});
+  res.render('tareas', { title: 'Tareas', tareas: tareasArray});
 });
 
 router.get('/tareas/new', function(req, res, next) {
@@ -15,36 +39,15 @@ router.get('/tareas/new', function(req, res, next) {
 });
 
 
-router.post('/tareas/crear', function(req, res) {
-  res.render('newTarea', { title: 'Crear Tarea'});
+router.post('/tareas/crear', (req, res) => {
+    var newTarea ={
+        "nom": req.body.nom,
+        "etiquetes": req.body.etiquetes,
+        "realitzacio": req.body.realitzacio        
+    }
+    tareasArray.push(newTarea);
+    res.redirect('/tareas');
 });
-
-
-
-
-var tareas = [
-{
-"nom":"Diseño wireframe",
-"etiquetes": [ "wireframe","app","hibrida"],
-"realització":"36"
-},
-{
-"nom":"Reunión",
-"etiquetes": ["app","hibrida"],
-"realització":"36"
-},
-{
-"nom":"Desarrollo Layout",
-"etiquetes": [ "layout","app","hibrida"],
-"realització":"36"
-},
-{
-"nom":"Reunión cliente",
-"etiquetes": [ "reunión","app","hibrida"],
-"realització":"36"
-}
-]
-
 
 
 module.exports = router;
